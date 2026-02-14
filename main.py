b71786f1fff5737992a87080c238464f6ac7fd81
Vladimir <VladimirNTS@github.com>
2026-02-14 05:52:13 +0400

    base project structure add

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import uvicorn
+
+
 def main():
-    print("Hello from telegram-bot-for-events!")
+    uvicorn.run(app='app.app:app', host="0.0.0.0", port=8000, reload=True)
 
 
 if __name__ == "__main__":
